83e575171e5f9375a5bfd5ac70e6d568e5108eff
Pavlo Podviaznikov <pavel.podviaznikov@gmail.com>
2023-03-26 13:27:50 +0200

    use db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'krakowdevs-clicker';
+  
+  conditions = {
+    isProcessing: true
+  };
+
+  clicker = {
+    email: "",
+    clicksNum: 0
+  };
+
+  constructor(
+    private AppService: AppService
+  ) {}
+
+  ngOnInit() {
+    this.AppService.getClicks("")
+      .subscribe((clicksNum: any) => {
+        this.clicker.clicksNum = clicksNum;
+
+        this.setProcessing(false);
+      });    
+  }
+
+  onGetUserClicks() {
+    
+  }
+
+  onClick() {
+    ++this.clicker.clicksNum;
+
+    this.AppService.saveClick(this.clicker.clicksNum)
+      .subscribe((clicksNum: any) => {
+        this.clicker.clicksNum = clicksNum;
+      });  
+  }
+
+  private setProcessing(bool: boolean) {
+    this.conditions.isProcessing = bool;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { AppService } from './app.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
